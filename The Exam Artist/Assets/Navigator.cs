b91c9420103f6c5d8be3c0a98e6840f0688a6f0b
Akshay1897 <akshaysbhosle@gmail.com>
2020-03-03 20:06:58 -0800

    adding vr feature to menu

---
+++ The Exam Artist/Assets/Navigator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+public class Navigator : MonoBehaviour
+{
+    public SteamVR_LaserPointer laserPointer;
+    public GameObject selectHandler;
+    private string clicked = "";
+
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+    }
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+    }
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+    }
+}
