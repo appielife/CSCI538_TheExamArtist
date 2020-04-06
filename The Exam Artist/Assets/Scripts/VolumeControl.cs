e89ca4a6860bd9a545bf7a2ae6f4ab503d1c9afe
DannyLin <bigbomb17@gmail.com>
2020-04-06 12:55:14 -0400

    add audio volume control

---
+++ The Exam Artist/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+    public Settings setting;
+    private AudioSource[] sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponents<AudioSource>();
+        setting = GameObject.Find("Settings").GetComponent<Settings>();
+        for(int i = 0; i < sound.Length; i++){
+            sound[i].volume = sound[i].volume * (((setting.getVolume() - 50) / 50) + 1);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
